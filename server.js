a884b6a7c00743dba0b2ab7f4c084de3cac89509
mcvxrhcc <mcvxrhcc@proton.me>
2024-11-17 20:30:49 +0700

    chart

---
+++ server.js
@@ -0,0 +1,47 @@
+import Fastify from 'fastify';
+import db from './database/db.js';
+import { readFile } from 'node:fs';
+import { join } from 'node:path';
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get('/', (_, reply) => {
+  const filePath = join('static', 'index.html');
+  console.log(filePath);
+  readFile(filePath, 'utf8', (err, content) => {
+    if (err) {
+      console.log('error');
+      reply.code(500).send('Error loading file');
+      return;
+    }
+    reply.code(200).type('text/html').send(content);
+    return;
+  });
+});
+
+fastify.get('/data', async (_, reply) => {
+  const data = db
+    .prepare(
+      `
+      SELECT 
+        coin, 
+        datetime (created_at, 'unixepoch', 'localtime') as timestamp,
+        ROW_NUMBER() OVER (PARTITION BY coin) as count
+      FROM volumes
+      WHERE
+        pings > 3
+        AND net_vol_percent > 0; 
+    `,
+    )
+    .all();
+
+  reply.type('application/json').code(200);
+  return data;
+});
+
+fastify.listen({ port: 3000 }, (err, address) => {
+  if (err) throw err;
+  console.log(`Server is now listening on ${address}`);
+});
